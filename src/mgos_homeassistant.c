7bfad312d8198dd113a0a5c11d01190117592e6b
Pim van Pelt <pim@ipng.nl>
2020-03-15 11:19:24 +0100

    Start fleshing out JSON configuration hierarchy -- start with provider.si7021 and add _si7021.[ch]

--- src/mgos_homeassistant.c
+++ src/mgos_homeassistant.c
@@ -17,6 +17,7 @@
 #include "mgos_homeassistant.h"
 
 #include "mgos.h"
+#include "mgos_homeassistant_si7021.h"
 
 bool mgos_homeassistant_fromfile(struct mgos_homeassistant *ha,
                                  const char *filename) {
@@ -25,7 +26,36 @@ bool mgos_homeassistant_fromfile(struct mgos_homeassistant *ha,
 
 bool mgos_homeassistant_fromjson(struct mgos_homeassistant *ha,
                                  const char *json) {
+  struct json_token val;
+  void *h = NULL;
+  int idx;
+
+  char *name = NULL;
+
   if (!ha || !json) return false;
 
-  return false;
+  // Set global config elements
+  json_scanf(json, strlen(json), "{name:%Q}", &name);
+  if (name) {
+    if (ha->node_name) free(ha->node_name);
+    ha->node_name = strdup(name);
+  }
+
+  // Read providers
+  while ((h = json_next_elem(json, strlen(json), h, ".provider.si7021", &idx,
+                             &val)) != NULL) {
+#ifdef MGOS_HAVE_SI7021_I2C
+    if (!mgos_homeassistant_si7021_fromjson(ha, val)) {
+      LOG(LL_WARN, ("Failed to add object from provider si7021, index %d, json "
+                    "follows:%.*s",
+                    idx, (int) val.len, val.ptr));
+    }
+#else
+    LOG(LL_ERROR, ("provider.si7021 config found: Add si7021-i2c to mos.yml, "
+                   "skipping .. "));
+#endif
+  }
+
+  if (name) free(name);
+  return true;
 }
---
+++ src/mgos_homeassistant_si7021.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2020 Pim van Pelt <pim@ipng.nl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef MGOS_HAVE_SI7021_I2C
+#include "mgos_homeassistant_si7021.h"
+
+static void si7021_timer(void *ud) {
+  struct mgos_homeassistant_object *o = (struct mgos_homeassistant_object *) ud;
+  if (!o) return;
+  mgos_homeassistant_object_send_status(o);
+}
+
+/*
+static void si7021_stat(struct mgos_homeassistant_object *o, struct json_out
+*json) { if (!o || !json) return; json_printf(json, "hoi wereld");
+}
+*/
+
+static void si7021_stat_humidity(struct mgos_homeassistant_object *o,
+                                 struct json_out *json) {
+  struct mgos_si7021 *sensor = NULL;
+  float humidity = NAN;
+
+  if (!o || !json) return;
+
+  sensor = (struct mgos_si7021 *) o->user_data;
+  if (!sensor) return;
+
+  humidity = mgos_si7021_getHumidity(sensor);
+  json_printf(json, "%.1f", humidity);
+}
+
+static void si7021_stat_temperature(struct mgos_homeassistant_object *o,
+                                    struct json_out *json) {
+  struct mgos_si7021 *sensor = NULL;
+  float temperature = NAN;
+
+  if (!o || !json) return;
+
+  sensor = (struct mgos_si7021 *) o->user_data;
+  if (!sensor) return;
+
+  temperature = mgos_si7021_getTemperature(sensor);
+  json_printf(json, "%.2f", temperature);
+}
+
+bool mgos_homeassistant_si7021_fromjson(struct mgos_homeassistant *ha,
+                                        struct json_token val) {
+  int i2caddr = -1;
+  int period = 60;
+  bool ret = false;
+  struct mgos_homeassistant_object *o = NULL;
+  struct mgos_si7021 *user_data = NULL;
+
+  if (!ha) goto exit;
+
+  json_scanf(val.ptr, val.len, "{i2caddr:%d,period:%d}", &i2caddr, &period);
+  user_data = mgos_si7021_create(mgos_i2c_get_global(), i2caddr);
+  if (!user_data) {
+    LOG(LL_ERROR, ("Could not create si7021 at i2caddr=%d", i2caddr));
+    goto exit;
+  }
+
+  o = mgos_homeassistant_object_add(ha, "si7021", COMPONENT_SENSOR, NULL, NULL,
+                                    user_data);
+  if (!o) {
+    LOG(LL_ERROR, ("Could not add si7021 to homeassistant"));
+    goto exit;
+  }
+
+  if (!mgos_homeassistant_object_class_add(o, "humidity", NULL,
+                                           si7021_stat_humidity)) {
+    LOG(LL_ERROR, ("Could not add 'humidity' class to si7021 object"));
+    goto exit;
+  }
+  if (!mgos_homeassistant_object_class_add(o, "temperature", NULL,
+                                           si7021_stat_temperature)) {
+    LOG(LL_ERROR, ("Could not add 'humidity' class to si7021 object"));
+    goto exit;
+  }
+
+  if (period > 0) mgos_set_timer(period * 1000, true, si7021_timer, o);
+
+  ret = true;
+exit:
+  return ret;
+}
+
+#endif  // MGOS_HAVE_SI7021_I2C
